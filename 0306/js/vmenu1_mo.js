c2a866caf7c810bf8810ff49572b56ee6e8e33df
eousink01 <eousink01@gmail.com>
2025-03-06 18:14:06 +0900

    0306

---
+++ 0306/js/vmenu1_mo.js
@@ -0,0 +1,20 @@
+$(function(){
+    // $('.sub').css('display','none');
+    $('.sub').hide();
+   // $('.sub').eq(0).show();
+
+    $('.m_menu li a').click(function(){
+        var kkk=$(this).next('.sub').css('display');    //css('display'): display 상태를 알려줌
+        // alert(kkk);
+       
+        if(kkk=='none'){
+            $('.sub').slideUp();
+            $(this).next('.sub').slideDown();
+
+        }else{
+            $('.sub').slideUp();
+        }
+        
+        return false;
+    });
+});
